b91405a922ee246a35d5047d3a439949487a3850
liuyqq <liuyq@cheok.com>
2019-01-10 14:34:40 +0800

    后置处理器

--- liuyq-center/src/main/java/DefaultListableBeanFactoryTest.java
+++ liuyq-center/src/main/java/DefaultListableBeanFactoryTest.java
@@ -1,18 +1,33 @@
+import com.liuyq.solr.domain.PrivateCarSearchDomain;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * Created by liuyq on 2018/12/28.
  */
 public class DefaultListableBeanFactoryTest {
 
+    AbstractApplicationContext applicationcontext=null;
+
+    @Before
+    public void before() {
+        System.out.println("》》》Spring ApplicationContext容器开始初始化了......");
+        applicationcontext= new ClassPathXmlApplicationContext(new String[]{"classpath:applicationContext.xml"});
+        System.out.println("》》》Spring ApplicationContext容器初始化完毕了......");
+    }
+
     @Test
     public void test(){
-        ClassPathResource resource = new ClassPathResource("applicationContext.xml");
-        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        reader.loadBeanDefinitions(resource);
+        FileSystemXmlApplicationContext resource = new FileSystemXmlApplicationContext("classpath:applicationContext.xml");
+        PrivateCarSearchDomain privateCarSearchDomain = (PrivateCarSearchDomain) resource.getBean("privateCarSearchDomain");
+        System.out.println(privateCarSearchDomain);
+    }
+
+    @Test
+    public void  test2() {
+        applicationcontext.registerShutdownHook();
     }
 }
---
+++ liuyq-center/src/main/java/com/liuyq/springtest/process/PostProcessorB.java
@@ -0,0 +1,37 @@
+package com.liuyq.springtest.process;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+
+/**
+ * Created by liuyq on 2019/1/10.
+ */
+public class PostProcessorB implements BeanPostProcessor, Ordered {
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        System.out.println("后置处理器2处理bean=【" + s + "】开始");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return o;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        System.out.println("后置处理器2处理bean=【"+s+"】完毕!");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return o;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
