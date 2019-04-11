f518b62ab1756ad945b6b86cc8a7d51066f48bd3
liuyqq <liuyq@cheok.com>
2019-04-11 20:51:09 +0800

    NameMathchMethodPointcut

--- liuyq-springtest/src/main/java/com/liuyq/springtest/aop/demo/test.java
+++ liuyq-springtest/src/main/java/com/liuyq/springtest/aop/demo/test.java
@@ -8,6 +8,7 @@ import com.liuyq.springtest.aop.service.Model1;
 import com.liuyq.springtest.aop.service.UserService;
 import org.junit.Test;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.util.PatternMatchUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,17 +31,26 @@ public class test {
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         String url = "[http://private4s.paodingcar.com:18080/download?fileID=8642033,http://private4s.paodingcar.com:18080/download?fileID=8642034,http://private4s.paodingcar.com:18080/download?fileID=8642035,http://private4s.paodingcar.com:18080/download?fileID=8642036,http://private4s.paodingcar.com:18080/download?fileID=8642037,http://private4s.paodingcar.com:18080/download?fileID=8642040,http://private4s.paodingcar.com:18080/download?fileID=8642041,http://private4s.paodingcar.com:18080/download?fileID=8642044,http://private4s.paodingcar.com:18080/download?fileID=8642046]";
-        List list = Arrays.asList(url.replace("[","").replace("]","").split(","));
+        List list = Arrays.asList(url.replace("[", "").replace("]", "").split(","));
         System.out.println(list.toString());
-        System.out.println(list.get(1)+","+list.get(2));
+        System.out.println(list.get(1) + "," + list.get(2));
     }
 
 
     @Test
-    public void test3(){
-        ISay iHello = (ISay) new SayHelloProxy().bind(new SayService(),new LogService());
+    public void test3() {
+        ISay iHello = (ISay) new SayHelloProxy().bind(new SayService(), new LogService());
         iHello.sayHello("liuyq");
+    }
+
+
+    @Test
+    public void test4() {
+        String[] patterns = {"save*", "update*ser", "delete*"};
+        boolean simpleMatch = PatternMatchUtils.simpleMatch(patterns, "updateUser");
+        System.out.println(simpleMatch);
+
     }
 }
